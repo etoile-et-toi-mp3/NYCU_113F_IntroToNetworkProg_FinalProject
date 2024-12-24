84671842815bc88f7a282cfda7d1972c0ee3e3f8
Eason <easonyeh71.cs12@nycu.edu.tw>
2024-12-24 17:49:27 +0800

    add debug print statement

--- UIED_Mahjong_server.c
+++ UIED_Mahjong_server.c
@@ -751,6 +751,7 @@ int hu_recursive_check(int *count, int n) {
 
 int hu_check(struct mj *decks, int nc) {
     // hu_check can sort the decks for its own purpose, but shouldn't modify the real deck;
+    printf("IN hu_check\n");
     struct mj carbon[17];
     memset(carbon, 0, 17 * sizeof(struct mj));
     memcpy(carbon, decks, (nc + 1) * sizeof(struct mj));
@@ -773,6 +774,7 @@ int hu_check(struct mj *decks, int nc) {
     int count[34] = {0}; // because there are totally 34 kinds of mjs in total (excluding FLOWER).
     for (int j = 0; j < nc + 1; ++j)
     {
+        printf("%d: %d\n",j,(carbon[j].type - 1) * 9 + carbon[j].number - 1);
         count[(carbon[j].type - 1) * 9 + carbon[j].number - 1]++;
     }
     for (int i = 0; i < 34; ++i)
@@ -1022,6 +1024,8 @@ int othersreaction(int *playernowp) {
             }
             players[*playernowp]->normal_capacity -= 3;
 
+            decks_quick_sort(players[*playernowp]->decks, 0, players[*playernowp]->normal_capacity - 1);
+
             write_message_wait_ack(players[(*playernowp + 1) % 4]->fd, "(Announce) player %d ponged it\n", *playernowp);
             write_message_wait_ack(players[(*playernowp + 2) % 4]->fd, "(Announce) player %d ponged it\n", *playernowp);
             write_message_wait_ack(players[(*playernowp + 3) % 4]->fd, "(Announce) player %d ponged it\n", *playernowp);
@@ -1073,6 +1077,8 @@ int othersreaction(int *playernowp) {
                 }
             }
             players[*playernowp]->normal_capacity -= 3;
+
+            decks_quick_sort(players[*playernowp]->decks, 0, players[*playernowp]->normal_capacity - 1);
 
             write_message_wait_ack(players[(*playernowp + 1) % 4]->fd, "(Announce) player %d ponged it\n", *playernowp);
             write_message_wait_ack(players[(*playernowp + 2) % 4]->fd, "(Announce) player %d ponged it\n", *playernowp);
@@ -1126,6 +1132,8 @@ int othersreaction(int *playernowp) {
             }
             players[*playernowp]->normal_capacity -= 3;
 
+            decks_quick_sort(players[*playernowp]->decks, 0, players[*playernowp]->normal_capacity - 1);
+
             write_message_wait_ack(players[(*playernowp + 1) % 4]->fd, "(Announce) player %d ponged it\n", *playernowp);
             write_message_wait_ack(players[(*playernowp + 2) % 4]->fd, "(Announce) player %d ponged it\n", *playernowp);
             write_message_wait_ack(players[(*playernowp + 3) % 4]->fd, "(Announce) player %d ponged it\n", *playernowp);
@@ -1152,9 +1160,9 @@ int othersreaction(int *playernowp) {
             {
                 write_message_wait_ack(players[(*playernowp + 1) % 4]->fd, "Type which 2 of the mjs you want to eat with: \n");
                 read_and_ack(players[(*playernowp + 1) % 4]->fd);
-
                 int eatindex1, eatindex2;
                 sscanf(recvline, "%d %d", &eatindex1, &eatindex2);
+
                 struct mj eat_temp[3];
                 eat_temp[0] = players[(*playernowp + 1) % 4]->decks[eatindex1];
                 eat_temp[1] = players[(*playernowp + 1) % 4]->decks[eatindex2];
@@ -1187,12 +1195,17 @@ int othersreaction(int *playernowp) {
 
             *playernowp = (*playernowp + 1) % 4;
 
+            decks_quick_sort(players[*playernowp]->decks, 0, players[(*playernowp + 1) % 4]->normal_capacity - 1);
+
             write_message_wait_ack(players[(*playernowp + 1) % 4]->fd, "(Announce) player %d ate it\n", *playernowp);
             write_message_wait_ack(players[(*playernowp + 2) % 4]->fd, "(Announce) player %d ate it\n", *playernowp);
             write_message_wait_ack(players[(*playernowp + 3) % 4]->fd, "(Announce) player %d ate it\n", *playernowp);
 
+            printf("IN is_hu\n");
             is_hu(*playernowp);
+            printf("In discard\n");
             discard(*playernowp);
+            printf("In othersreaction\n");
             othersreaction(playernowp);
         }
         else
